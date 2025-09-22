385970dff01dd1975d3765b66fa29e18021d23b1
rafaelrae <delazerirafael5@gmail.com>
2025-09-22 04:38:17 -0300

    Ongoing

--- exercicios-javascript-1/scripts/exerc5.js
+++ exercicios-javascript-1/scripts/exerc5.js
@@ -1,1 +1,8 @@
 let numeroEscolhido = prompt("Escolha um número de 1 a 10  (ง ͠° ͟ل͜ ͡°)ง");
+for (let counter = 1; counter < (10+1); counter++) {
+    let resultado = numeroEscolhido * counter;
+    console.log(`${numeroEscolhido} x ${counter} = ${resultado}`);
+    let linhaTabuada = `${numeroEscolhido} x ${counter} = ${resultado}<br>`;
+    resultadoTabuada.innerHTML += linhaTabuada;
+}
+numeroEscolhidoTabuada.innerHTML += numeroEscolhido;
